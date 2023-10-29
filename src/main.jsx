79ebb55ed41260fe55066ad45497d9d55d619dc0
ymillerr <millerandvitor@gmail.com>
2023-10-29 18:44:58 +0100

    feat: remove App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { theme } from './styles/theme/default'
+
+import { Router } from './routes'
+
+import { AuthContextProvider } from './context/AuthContext'
+import { ProductsContextProvider } from './context/ProductsContext'
+import { NotificationContextProvider } from './context/NotificationContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+
+      <NotificationContextProvider>
+        <AuthContextProvider>
+          <ProductsContextProvider>
+            <Router />
+          </ProductsContextProvider>
+        </AuthContextProvider>
+      </NotificationContextProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
